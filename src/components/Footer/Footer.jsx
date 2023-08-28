d21b4cf271a8ec439994ed0e5792aeb818689f2e
joseaot <josealbertoaot@gmail.com>
2023-08-28 03:18:10 -0300

    try77

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,18 +5,53 @@ import { IoLogoWhatsapp } from "react-icons/io";
 const Footer = () => {
   return (
     <div className="container">
-      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 border-top text-center text-md-left">
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-2 border-top text-center text-md-left">
         <div className="col-md-4 d-flex align-items-center">
-          <a href="/" className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+          <a
+            href="/"
+            className="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
+          >
             <svg className="bi" width="30" height="24"></svg>
           </a>
-          <span className="mb-3 mb-md-0 text-muted text-sm">&copy; 2023 Con Cariño Jose O.</span>
+          <span
+            className="mb-3 mb-md-0 text-muted text-sm"
+            style={{ fontSize: "20px" }}
+          >
+            &copy; 2023 Con Cariño Jose O.
+          </span>
         </div>
 
         <ul className="nav col-md-4 justify-content-center list-unstyled d-flex">
-          <li className="ms-3"><a className="text-muted" href="https://www.facebook.com/josealberto.orellana.5" target="_blank" rel="noopener noreferrer"><BsFacebook /></a></li>
-          <li className="ms-3"><a className="text-muted" href="https://w.app/yl0cL7" target="_blank" rel="noopener noreferrer"><IoLogoWhatsapp /></a></li>
-          <li className="ms-3"><a className="text-muted" href="https://www.instagram.com/joseaot/" target="_blank" rel="noopener noreferrer"><AiFillInstagram /></a></li>
+          <li className="mx-2">
+            <a
+              className="text-muted"
+              href="https://www.facebook.com/josealberto.orellana.5"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <BsFacebook />
+            </a>
+          </li>
+          <li className="mx-2">
+            <a
+              className="text-muted"
+              href="https://w.app/yl0cL7"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <IoLogoWhatsapp />
+            </a>
+          </li>
+          <li className="mx-2">
+            <a
+              className="text-muted"
+              href="https://www.instagram.com/joseaot/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <AiFillInstagram />
+            </a>
+          </li>
         </ul>
       </footer>
     </div>
